9f6e90422f699e18ab5d43efe7c33933ad6d3249
Dmitry Utkin <dmitry.utkin@gmail.com>
2015-01-28 02:52:07 +0200

    up a bit

--- utils.js
+++ utils.js
@@ -2,44 +2,44 @@
  * Logging util
  */
 function _log() {;
-	try {
-		for (var i = 0; i < arguments.length; i++) {
-			console.log(arguments[i]);
-		}
-	} catch(e) {}
+  try {
+    for (var i = 0; i < arguments.length; i++) {
+      console.log(arguments[i]);
+    }
+  } catch(e) {}
 }
 /**
  * Warnings Logging util
  */
 function _warn() {
-	try{
-		for (var i = 0; i < arguments.length; i++) {
-			console.warn(arguments[i]);
-		}
-	} catch(e) {}
+  try{
+    for (var i = 0; i < arguments.length; i++) {
+      console.warn(arguments[i]);
+    }
+  } catch(e) {}
 }
 
 /**
  * Errors Logging util
  */
 function _err() {
-	try {
-		for (var i = 0; i < arguments.length; i++) {
-			console.error(arguments[i]);
-		}
-	} catch(e){
-		//alert(msg);
-	}
+  try {
+    for (var i = 0; i < arguments.length; i++) {
+      console.error(arguments[i]);
+    }
+  } catch(e){
+    //alert(msg);
+  }
 }
 
 function enableLogs() {
-	window.log = _log;
-	window.warn = _warn;
-	window.err = _err;
+  window.log = _log;
+  window.warn = _warn;
+  window.err = _err;
 }
 
 function disableLogs() {
-	window.log = function () {};
-	window.warn = function () {};
-	window.err = function () {};
+  window.log = function () {};
+  window.warn = function () {};
+  window.err = function () {};
 }
